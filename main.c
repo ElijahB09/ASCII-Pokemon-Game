03fc1ac965207a4fd1172e50e9847067fe019eaa
Elijah Brady <bradyeli09@gmail.com>
2023-08-31 14:48:26 -0500

    Weird bug happening right now, might have a memory problem

--- main.c
+++ main.c
@@ -2,21 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct {
+    int x_coord;
+    int y_coord;
+    int elevation;
+    char terrainPiece;
+} terrainCell;
+
 int main(int argc, char *argv[]) {
-    char map[21][80];
-    int elevationMap[21][80];
+    terrainCell map[21][80];
     int i;
     int j;
     int k;
     int rand_x_coords[10];
     int rand_y_coords[10];
-    srand((unsigned int) time(NULL));
+    time_t t;
+
+    srand(time(&t));
 
     // Generate 10 random points on the map between 1 - 19 and 1 - 78 exclusive
     for (i = 0; i < 10; i++) {
         rand_x_coords[i] = (rand() % 77) + 1;
         rand_y_coords[i] = (rand() % 18) + 1;
     }
+
     for (i = 0; i < 10; i++) {
         printf("x-coord: %d\n", rand_x_coords[i]);
         printf("y-coord: %d\n", rand_y_coords[i]);
@@ -24,9 +33,11 @@ int main(int argc, char *argv[]) {
 
     for (i = 0; i < 21; i++) {
         for (j = 0; j < 80; j++) {
+            map[i][j].x_coord = j;
+            map[i][j].y_coord = i;
             if (i == 0 || j == 0 || i == 20 || j == 79) {
-                map[i][j] = '%';
-                elevationMap[i][j] = 9;
+                map[i][j].terrainPiece = '%';
+                map[i][j].elevation = 9;
             } else {
                 for (k = 0; k < 10; k++) {
                     // Need to randomize terrain, there are :, ^, %, ., and ~
@@ -34,28 +45,29 @@ int main(int argc, char *argv[]) {
                     if (j == rand_x_coords[k] && i == rand_y_coords[k]) {
                         switch (k % 5) { // Should go between 0 - 4
                             case 0:
-                                map[i][j] = ':';
-                                elevationMap[i][j] = 1;
+                                map[i][j].terrainPiece = ':';
+                                map[i][j].elevation = 2;
                                 break;
                             case 1:
-                                map[i][j] = '^';
-                                elevationMap[i][j] = 2;
+                                map[i][j].terrainPiece = '^';
+                                map[i][j].elevation = 3;
                                 break;
                             case 2:
-                                map[i][j] = '%';
-                                elevationMap[i][j] = 9;
+                                map[i][j].terrainPiece = '%';
+                                map[i][j].elevation = 9;
                                 break;
                             case 3:
-                                map[i][j] = '~';
-                                elevationMap[i][j] = 4;
+                                map[i][j].terrainPiece = '~';
+                                map[i][j].elevation = 5;
                                 break;
                             case 4:
-                                map[i][j] = '.';
-                                elevationMap[i][j] = 5;
+                                map[i][j].terrainPiece = '.';
+                                map[i][j].elevation = 1;
+                                break;
                         }
-                    } else if (map[i][j] != ':' && map[i][j] != '^' && map[i][j] != '%' && map[i][j] != '~' && map[i][j] != '.') {
-                        map[i][j] = '_';
-                        elevationMap[i][j] = 0;
+                    } else if (map[i][j].terrainPiece != ':' && map[i][j].terrainPiece != '^' && map[i][j].terrainPiece != '%' && map[i][j].terrainPiece != '~' && map[i][j].terrainPiece != '.') {
+                        map[i][j].terrainPiece = '_';
+                        map[i][j].elevation = 0;
                     }
                 }
             }
@@ -64,14 +76,7 @@ int main(int argc, char *argv[]) {
 
     for (i = 0; i < 21; i++) {
         for (j = 0; j < 80; j++) {
-            printf("%c", map[i][j]);
-        }
-        printf("\n");
-    }
-
-    for (i = 0; i < 21; i++) {
-        for (j = 0; j < 80; j++) {
-            printf("%d", elevationMap[i][j]);
+            printf("%c", map[i][j].terrainPiece);
         }
         printf("\n");
     }
