8a94f7b3d963d3c4688cbed9fd8176352a685f95
Elijah Brady <bradyeli09@gmail.com>
2023-08-31 12:08:53 -0500

    Added comments for readability

--- main.c
+++ main.c
@@ -28,6 +28,7 @@ int main(int argc, char *argv[]) {
                 map[i][j] = '%';
             } else {
                 for (k = 0; k < 10; k++) {
+                    // A # symbol for now denotes a seed for terrain
                     if (j == rand_x_coords[k] && i == rand_y_coords[k]) {
                         map[i][j] = '#';
                     } else if (map[i][j] != '#') {
