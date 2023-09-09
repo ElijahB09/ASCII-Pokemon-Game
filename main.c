e958cea024116094fdbbd06101732e9bc420a427
Elijah Brady <bradyeli09@gmail.com>
2023-09-09 00:47:35 -0500

    It kinda works, some kinks to work out lol but yea, yay!

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define QUEUE_SIZE 4096
+#define QUEUE_SIZE 2048
 #define HEAP_SIZE 8192
 #define NUM_RAN_COORDS 30
 #define MAX_ELEVATION 99
+#define INFINITY 8192
 
 typedef struct {
-    int x_coord, y_coord, elevation, distance, previous_x, previous_y;
+    int x_coord, y_coord, elevation, previous_x, previous_y, visited;
     char terrainPiece;
+    long distance;
 } terrainCell;
 
 typedef struct {
@@ -18,11 +20,12 @@ typedef struct {
 } Queue;
 
 typedef struct {
-    terrainCell *arr;
-    int size, capacity;
+    terrainCell * arr;
+    int size;
+    unsigned capacity;
 } MinHeap;
 
-MinHeap* buildMinHeap(int capacity) {
+MinHeap* buildMinHeap(unsigned capacity) {
     MinHeap* heap = (MinHeap *)malloc(sizeof(MinHeap));
 
     if (heap == NULL) {
@@ -45,7 +48,7 @@ MinHeap* buildMinHeap(int capacity) {
 void insertHelper(MinHeap* heap, int index) {
     int parent = (index - 1) / 2;
 
-    if (heap->arr[parent].elevation > heap->arr[index].elevation) {
+    if (heap->arr[parent].distance > heap->arr[index].distance) {
         terrainCell temp = heap->arr[parent];
         heap->arr[parent] = heap->arr[index];
         heap->arr[index] = temp;
@@ -73,9 +76,9 @@ void minHeapify(MinHeap * heap, int index)
     if (right >= heap->size || right < 0)
         right = -1;
 
-    if (left != -1 && heap->arr[left].elevation < heap->arr[index].elevation)
+    if (left != -1 && heap->arr[left].distance < heap->arr[index].distance)
         min = left;
-    if (right != -1 && heap->arr[right].elevation < heap->arr[index].elevation)
+    if (right != -1 && heap->arr[right].distance < heap->arr[index].distance)
         min = right;
 
     if (min != index) {
@@ -114,7 +117,7 @@ Queue* createQueue(unsigned capacity)
     // This is important, see the enqueue
     queue->rear = capacity - 1;
     queue->array = (terrainCell *)malloc(
-            queue->capacity * sizeof(int));
+            queue->capacity * sizeof(terrainCell));
     return queue;
 }
 
@@ -164,69 +167,107 @@ terrainCell rear(Queue* queue)
 
 int getNeighbors(int x, int y, terrainCell map[y][x], terrainCell cell, terrainCell* neighbors) {
     int numNeighbors;
-
-    numNeighbors = 1;
-    if (cell.x_coord == 0) {
+    if (cell.x_coord == 0 && cell.y_coord == 0) {
         neighbors[0] = map[cell.y_coord][cell.x_coord + 1];
+        neighbors[1] = map[cell.y_coord + 1][cell.x_coord];
+        numNeighbors = 2;
+    } else if (cell.x_coord == 79 && cell.y_coord == 0) {
+        neighbors[0] = map[cell.y_coord][cell.x_coord - 1];
+        neighbors[1] = map[cell.y_coord + 1][cell.x_coord];
+        numNeighbors = 2;
+    } else if (cell.x_coord == 0 && cell.y_coord == 20) {
+        neighbors[0] = map[cell.y_coord][cell.x_coord + 1];
+        neighbors[1] = map[cell.y_coord - 1][cell.x_coord];
+        numNeighbors = 2;
+    } else if (cell.x_coord == 79 && cell.y_coord == 20) {
+        neighbors[0] = map[cell.y_coord][cell.x_coord - 1];
+        neighbors[1] = map[cell.y_coord - 1][cell.x_coord];
+        numNeighbors = 2;
+    } else if (cell.x_coord == 0) {
+        neighbors[0] = map[cell.y_coord][cell.x_coord + 1];
+        neighbors[1] = map[cell.y_coord - 1][cell.x_coord];
+        neighbors[2] = map[cell.y_coord + 1][cell.x_coord];
+        numNeighbors = 3;
     } else if (cell.y_coord == 0) {
         neighbors[0] = map[cell.y_coord + 1][cell.x_coord];
+        neighbors[1] = map[cell.y_coord][cell.x_coord + 1];
+        neighbors[2] = map[cell.y_coord][cell.x_coord - 1];
+        numNeighbors = 3;
     } else if (cell.x_coord == 79) {
         neighbors[0] = map[cell.y_coord][cell.x_coord - 1];
+        neighbors[1] = map[cell.y_coord - 1][cell.x_coord];
+        neighbors[2] = map[cell.y_coord + 1][cell.x_coord];
+        numNeighbors = 3;
     } else if (cell.y_coord == 20) {
         neighbors[0] = map[cell.y_coord - 1][cell.x_coord];
+        neighbors[1] = map[cell.y_coord][cell.x_coord + 1];
+        neighbors[2] = map[cell.y_coord][cell.x_coord - 1];
+        numNeighbors = 3;
     } else {
         neighbors[0] = map[cell.y_coord][cell.x_coord + 1];
-        neighbors[0] = map[cell.y_coord][cell.x_coord - 1];
-        neighbors[0] = map[cell.y_coord + 1][cell.x_coord];
-        neighbors[0] = map[cell.y_coord - 1][cell.x_coord];
+        neighbors[1] = map[cell.y_coord][cell.x_coord - 1];
+        neighbors[2] = map[cell.y_coord + 1][cell.x_coord];
+        neighbors[3] = map[cell.y_coord - 1][cell.x_coord];
         numNeighbors = 4;
     }
 
     return numNeighbors;
 }
 
-void Dijkstra(int x, int y, terrainCell map[y][x], int elevations[y][x], terrainCell start, terrainCell end) {
+void Dijkstra(int x, int y, terrainCell map[y][x], terrainCell start, terrainCell end) {
     int i, j, numNeighbors, tempDistance;
-    MinHeap *heap = malloc(sizeof (MinHeap));
-    terrainCell *temp = malloc(sizeof (terrainCell));
-    terrainCell (*neighbors) = malloc(sizeof (terrainCell[4]));
+    MinHeap *heap;
+    terrainCell temp;
+    terrainCell neighbors[4];
 
     for (i = 0; i < y; i++) {
         for (j = 0; j < x; j++) {
-            map[i][j].distance = 9999;
+            map[i][j].distance = INFINITY;
+            map[i][j].visited = 0;
+            map[i][j].previous_x = -1;
+            map[i][j].previous_y = -1;
         }
     }
-    start.distance = 0;
+    map[start.y_coord][start.x_coord].distance = 0;
 
     heap = buildMinHeap(HEAP_SIZE);
-    insert(heap, start);
-
+    insert(heap, map[start.y_coord][start.x_coord]);
     while (heap->size != 0) {
-        *temp = extractMin(heap);
-        if (temp == &end) {
-            while (temp != &start) {
-                map[temp->previous_y][temp->previous_x].elevation = 0;
-                map[temp->previous_y][temp->previous_x].terrainPiece = '#';
-                *temp = map[temp->previous_y][temp->previous_x];
-            }
+        temp = extractMin(heap);
+        if ((temp.x_coord == end.x_coord) && (temp.y_coord == end.y_coord)) {
             break;
         }
-        numNeighbors = getNeighbors(x, y, map, *temp, neighbors);
-        for (i = 0; i < numNeighbors; i ++) {
-            tempDistance = temp->distance + neighbors[i].elevation;
-            if (tempDistance < neighbors[i].distance) {
-                neighbors[i].distance = tempDistance;
-                neighbors[i].previous_x = temp->x_coord;
-                neighbors[i].previous_y = temp->y_coord;
-                insert(heap, neighbors[i]);
+        if (map[temp.y_coord][temp.x_coord].visited == 1) {
+            continue;
+        }
+        map[temp.y_coord][temp.x_coord].visited = 1;
+        numNeighbors = getNeighbors(x, y, map, temp, neighbors);
+        for (i = 0; i < numNeighbors; i++) {
+            if (map[neighbors[i].y_coord][neighbors[i].x_coord].visited == 0) {
+                tempDistance = temp.distance + neighbors[i].elevation;
+                if (tempDistance < neighbors[i].distance) {
+                    map[neighbors[i].y_coord][neighbors[i].x_coord].distance = tempDistance;
+                    map[neighbors[i].y_coord][neighbors[i].x_coord].previous_x = temp.x_coord;
+                    map[neighbors[i].y_coord][neighbors[i].x_coord].previous_y = temp.y_coord;
+                    insert(heap, map[neighbors[i].y_coord][neighbors[i].x_coord]);
+                }
             }
         }
     }
+    printf("Finished Path finding\n\n");
+    temp = map[end.y_coord][end.x_coord];
+    printf("x-coord: %d\ny-coord: %d\nprevious-x: %d\nprevious-y: %d\n\n", temp.x_coord, temp.y_coord, temp.previous_x, temp.previous_y);
+    while ((temp.x_coord != start.x_coord) && (temp.y_coord != start.y_coord)) {
+        map[temp.previous_y][temp.previous_x].elevation = 0;
+        map[temp.previous_y][temp.previous_x].terrainPiece = '#';
+        temp = map[temp.previous_y][temp.previous_x];
+    }
+
+    free(heap);
 }
 
 int main(int argc, char *argv[]) {
-    terrainCell (*map)[80] = malloc(sizeof (terrainCell[21][80]));
-    int (*elevations)[80] = malloc(sizeof (int[21][80]));
+    terrainCell (*map)[80] = malloc(sizeof (terrainCell) * 21 * 80);
     terrainCell (*randomCells) = malloc(sizeof (terrainCell[NUM_RAN_COORDS]));
     terrainCell (*currentCell) = malloc(sizeof (terrainCell));
     int currentCellXCoord;
@@ -256,10 +297,10 @@ int main(int argc, char *argv[]) {
             map[i][j].y_coord = i;
             // Using default values for each cell of _ and 0
             map[i][j].terrainPiece = '_';
-            map[i][j].elevation = elevations[i][j] = 0;
+            map[i][j].elevation = 0;
             if (i == 0 || j == 0 || i == 20 || j == 79) {
                 map[i][j].terrainPiece = '%';
-                map[i][j].elevation = elevations[i][j] = MAX_ELEVATION;
+                map[i][j].elevation = MAX_ELEVATION;
             } else {
                 for (k = 0; k < NUM_RAN_COORDS; k++) {
                     // Need to randomize terrain, there are :, ^, %, ., and ~
@@ -268,32 +309,32 @@ int main(int argc, char *argv[]) {
                         switch (k % 6) { // Should go between 0 - 5
                             case 0:
                                 map[i][j].terrainPiece = ':';
-                                map[i][j].elevation = elevations[i][j] = 2;
+                                map[i][j].elevation = 2;
                                 randomCells[k] = map[i][j];
                                 break;
                             case 1:
                                 map[i][j].terrainPiece = '^';
-                                map[i][j].elevation = elevations[i][j] = 3;
+                                map[i][j].elevation = 3;
                                 randomCells[k] = map[i][j];
                                 break;
                             case 2:
                                 map[i][j].terrainPiece = '.';
-                                map[i][j].elevation = elevations[i][j] = 1;
+                                map[i][j].elevation = 1;
                                 randomCells[k] = map[i][j];
                                 break;
                             case 3:
                                 map[i][j].terrainPiece = '.';
-                                map[i][j].elevation = elevations[i][j] = 1;
+                                map[i][j].elevation = 1;
                                 randomCells[k] = map[i][j];
                                 break;
                             case 4:
                                 map[i][j].terrainPiece = '~';
-                                map[i][j].elevation = elevations[i][j] = 5;
+                                map[i][j].elevation = 5;
                                 randomCells[k] = map[i][j];
                                 break;
                             case 5:
                                 map[i][j].terrainPiece = '%';
-                                map[i][j].elevation = elevations[i][j] = 6;
+                                map[i][j].elevation = 6;
                                 randomCells[k] = map[i][j];
                                 break;
                         }
@@ -313,7 +354,7 @@ int main(int argc, char *argv[]) {
             for (j = 0; j < 3; j++) {
                 if (map[currentCellYCoord + (i - 1)][currentCellXCoord + (j - 1)].terrainPiece == '_') {
                     map[currentCellYCoord + (i - 1)][currentCellXCoord + (j - 1)].terrainPiece = currentCell->terrainPiece;
-                    map[currentCellYCoord + (i - 1)][currentCellXCoord + (j - 1)].elevation = elevations[i][j] = currentCell->elevation;
+                    map[currentCellYCoord + (i - 1)][currentCellXCoord + (j - 1)].elevation = currentCell->elevation;
                     enqueue(seeding_queue, map[currentCellYCoord + (i - 1)][currentCellXCoord + (j - 1)]);
                 }
             }
@@ -326,20 +367,27 @@ int main(int argc, char *argv[]) {
     rand_path_down = (rand() % 77) + 1;
 
     map[rand_path_left][0].terrainPiece = '#';
-    map[rand_path_left][0].elevation = elevations[rand_path_left][0] = 0;
+    map[rand_path_left][0].elevation = 0;
 
     map[rand_path_right][79].terrainPiece = '#';
-    map[rand_path_right][79].elevation = elevations[rand_path_right][79] = 0;
+    map[rand_path_right][79].elevation = 0;
 
     map[0][rand_path_up].terrainPiece = '#';
-    map[0][rand_path_up].elevation = elevations[0][rand_path_up] = 0;
+    map[0][rand_path_up].elevation = 0;
 
     map[20][rand_path_down].terrainPiece = '#';
-    map[20][rand_path_down].elevation = elevations[20][rand_path_down] = 0;
+    map[20][rand_path_down].elevation = 0;
 
     printf("Left: %d, Right: %d, Up: %d, Down: %d\n", rand_path_left, rand_path_right, rand_path_up, rand_path_down);
 
-//    Dijkstra(80, 21, map, elevations, map[rand_path_left][0], map[rand_path_right][79]);
+    for (i = 0; i < 21; i++) {
+        for (j = 0; j < 80; j++) {
+            printf("%c", map[i][j].terrainPiece);
+        }
+        printf("\n");
+    }
+
+    Dijkstra(80, 21, map, map[rand_path_left][0], map[rand_path_right][79]);
 
     for (i = 0; i < 21; i++) {
         for (j = 0; j < 80; j++) {
@@ -349,9 +397,9 @@ int main(int argc, char *argv[]) {
     }
 
     free(map);
-    free(elevations);
     free(randomCells);
     free(currentCell);
+    free(seeding_queue);
 
     return 0;
 }
